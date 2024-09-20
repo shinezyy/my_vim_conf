23de67de3d62f6199c1bdd50bfb96c8844322d8b
Zhou Yaoyang <zhouyaoyang17@mails.ucas.edu.cn>
2024-09-20 17:32:14 +0800

    track colorscheme

---
+++ neovim/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add badwolf
+  { "sjl/badwolf" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "badwolf",
+    },
+  },
+}
